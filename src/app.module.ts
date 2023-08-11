7ff32aa6e66fed53c3aa47de84938c3a3fab5811
paikpaik <woduf9311@gmail.com>
2023-08-12 01:46:36 +0900

    Add: mongoose로 mongodb연결

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductModule } from './product/product.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [ProductModule],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/MISC'),
+    ProductModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
