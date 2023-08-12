c507b81dc0b90d0c154a1b68ff052c3d30d5acee
paikpaik <woduf9311@gmail.com>
2023-08-13 01:49:22 +0900

    Add: auth register api

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,24 @@
 import { Injectable } from '@nestjs/common';
 import { UserService } from 'src/user/user.service';
+import * as bcrypt from 'bcrypt';
+import { NewUserDTO } from 'src/user/dtos/new-user.dto';
+import { UserDetails } from 'src/user/user-details.interface';
 
 @Injectable()
 export class AuthService {
   constructor(private userService: UserService) {}
+
+  async hashPassword(password: string): Promise<string> {
+    return bcrypt.hash(password, 12);
+  }
+
+  async register(user: Readonly<NewUserDTO>): Promise<UserDetails | any> {
+    const { name, email, password } = user;
+    const existingUser = await this.userService.findByEmail(email);
+    if (existingUser) return '이미 존재하는 Email입니다!';
+
+    const hashedPassword = await this.hashPassword(password);
+    const newUser = await this.userService.create(name, email, hashedPassword);
+    return this.userService._getUserDetails(newUser);
+  }
 }
