688bb8f633ae0bc63087328a267a0ee417b6e97a
paikpaik <woduf9311@gmail.com>
2023-08-13 10:35:21 +0900

    Add: login api를 위한 auth module에 jwtmodule 연결

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,9 +2,18 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UserModule } from 'src/user/user.module';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    UserModule,
+    JwtModule.registerAsync({
+      useFactory: () => ({
+        secret: 'secret',
+        signOptions: { expiresIn: '3600s' },
+      }),
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
