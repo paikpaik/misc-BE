6af47fb530e4c04d9dfc573964f31e155ac6ef11
paikpaik <woduf9311@gmail.com>
2023-08-25 17:51:21 +0900

    Add: stripe checkout service 코드 추가

--- src/stripe/stripe.service.ts
+++ src/stripe/stripe.service.ts
@@ -1,4 +1,27 @@
+import { Cart } from './Cart.model';
 import { Injectable } from '@nestjs/common';
+import Stripe from 'stripe';
 
 @Injectable()
-export class StripeService {}
+export class StripeService {
+  private stripe;
+
+  constructor() {
+    this.stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+      apiVersion: '2023-08-16',
+    });
+  }
+
+  checkout(cart: Cart) {
+    const totalPrice = cart.reduce(
+      (acc, item) => acc + item.quantity * item.price,
+      0,
+    );
+
+    return this.stripe.paymentIntents.create({
+      amount: totalPrice * 100, // cents
+      currency: 'usd', // set currency
+      payment_method_types: ['card'],
+    });
+  }
+}
