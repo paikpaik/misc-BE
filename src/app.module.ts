531d321db281d52c819a2159a178e7f292b81f0c
paikpaik <woduf9311@gmail.com>
2023-08-25 15:35:31 +0900

    Init: stripe 설치 및 모듈, 컨트롤러, 서비스 테스트코드 없이 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { ProductModule } from './product/product.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { StripeModule } from './stripe/stripe.module';
 
 @Module({
   imports: [
@@ -12,6 +13,7 @@ import { AuthModule } from './auth/auth.module';
     ProductModule,
     UserModule,
     AuthModule,
+    StripeModule,
   ],
   controllers: [AppController],
   providers: [AppService],
